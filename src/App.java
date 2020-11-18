71d3c6ea8bd8bf05377ab5cb1869703563b8879f
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-11-18 14:33:44 -0500

    monka5

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String[] lineas=new String[];
+        String path ="";
         System.out.println("Hello, World!");
         ReadFile fileReader = new ReadFile();
         lineas=fileReader.readFile(path);
