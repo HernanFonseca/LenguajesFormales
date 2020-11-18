4fada378d3741fb79e9ca1845a117bb556b618c4
Edulinaro <edulinaro@gmail.com>
2020-11-18 12:36:17 -0500

    TEST

--- src/App.java
+++ src/App.java
@@ -1,10 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
         int a;
         int b;
         System.out.println("Hello");
-
+        System.out.println("TEST");
     }
 }
