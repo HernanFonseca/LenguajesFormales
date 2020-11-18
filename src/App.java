481eb46c2c4e4546f478ab7b3b0d30616442b486
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-11-18 18:03:52 -0500

    Ya debería funcionar leyendo palabra por palabra

--- src/App.java
+++ src/App.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        List<String> lineas=new ArrayList<String>();
+        List<String> palabras=new ArrayList<String>();
         String path ="";
         System.out.println("Hello, World!");
         ReadFile fileReader = new ReadFile();
-        lineas=fileReader.ReadTxt(path);
-        
+        palabras=fileReader.ReadTxt(path);
     }
 }
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -4,14 +4,16 @@ import java.util.*;
 public class ReadFile{
     public List<String> ReadTxt(String path) {
         List<String> input=new ArrayList<String>();
-
+        String [] palabras= new String[]{};
         try {
             File file = new File(path);
             Scanner reader = new Scanner(file);
             while (reader.hasNextLine()) {
                 String data = reader.nextLine();
-                
-                input.add(data);
+                palabras=data.split(" ");
+                for (String palabra : palabras) {
+                    input.add(palabra);
+                }
             }
             reader.close();
         } catch (Exception e) {
