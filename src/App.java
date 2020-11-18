34c3f2bd49bbfa27eafada8e8246e5671a43c5b8
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-11-18 18:07:57 -0500

    Tested Funciona

--- src/App.java
+++ src/App.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         List<String> palabras=new ArrayList<String>();
-        String path ="";
-        System.out.println("Hello, World!");
+        String path ="./test.txt";
+
         ReadFile fileReader = new ReadFile();
         palabras=fileReader.ReadTxt(path);
+        System.out.println(palabras);
     }
 }
