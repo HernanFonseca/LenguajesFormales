6688bf954301e347885db134b47898becf417fa8
Edulinaro <edulinaro@gmail.com>
2020-11-18 16:17:24 -0500

    Nombre y Telefono Done

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,15 +1,19 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * Analizador Lexico para Formulario que tenemos
  */
 public class Lexer {
+
     public static enum Type {
         // Los tipos de Token que se tienen
         NOMBRE, EMAIL, TELEFONO,
         CONTNOMBRE, CONTEMAIL, CONTTELEFONO;
     }
+
     public static class Token {
         public final Type t;
         public final String c; // contents
@@ -50,16 +54,34 @@ public class Lexer {
         return result;
     }
 
+    public static Boolean lexName(String input){
+        String pattern = "[a-z|A-Z]+";
+        boolean matches = Pattern.matches(pattern, input);
+        return matches;
+    }
+
+    public static Boolean lexTelephone(String input){
+        String pattern = "[6][0-9]{3}[-][0-9]{4}";
+        boolean matches = Pattern.matches(pattern, input);
+        return matches;
+    }
+
+
     
+
     //Esta funcion se llama para cada palabra del read input que hernan hizo
-    public static Token tokenize(String input){
+    public static String tokenize(String input){
+        String type = null;
         Boolean reservedEmail = lexReservedEmail(input);
         Boolean reservedPhone = lexReservedTelephone(input); 
         Boolean reservedName = lexReservedName(input);
+        Boolean name = lexName(input);
+        //Boolean email = lexEmail(input);
+        //Boolean telephone = lexTelephone(input);
 
         //Aqui va el if comparando todos estos bool y el que sea true asigna el input a ese token
-
-
+        return type;
+        
 
     }
 }
