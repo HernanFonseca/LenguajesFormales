4ee12d53d76b9bf3690336dcbc5c8a3eb4a0d30f
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-11-18 14:32:55 -0500

    Espero que esto lea

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        String[] lineas=new String[];
         System.out.println("Hello, World!");
+        ReadFile fileReader = new ReadFile();
+        lineas=fileReader.readFile(path);
     }
 }
