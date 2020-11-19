8799628bea3018cdc89123c0d80ca2f2471f897e
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-11-18 23:40:24 -0500

    Ahora si

--- src/App.java
+++ src/App.java
@@ -5,6 +5,15 @@ import java.util.*;
 
 public class App {
 
+    public static class Token {
+        public final String t;
+        public final String c; // contents
+
+        public Token(String t, String c) {
+            this.t = t;
+            this.c = c;
+        }
+    }
     public static void main(String[] args) throws Exception {
 
             String pattern = "[a-z]{1}[a-z|0-9|_|-]*[@]" + ".*(hotmail|gmail|outlook|yahoo).*{1}[.]" + ".*(es|com|net|org|edu).*{1}";
