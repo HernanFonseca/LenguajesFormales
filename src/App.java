2abd81871cd0337bf0fa50ebbe6de530823e032c
NintakuSalads <lipx31@hotmail.com>
2020-11-18 23:20:33 -0600

    mensaje

--- src/App.java
+++ src/App.java
@@ -1,44 +1,24 @@
-//import java.util.regex.*;
+
+import java.util.regex.*;
+
 import java.util.*;
 
 public class App {
 
-
-    public static class Token {
-        public final String t;
-        public final String c; // contents
-
-        public Token(String t, String c) {
-            this.t = t;
-            this.c = c;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
 
-        List<String> palabras = new ArrayList<String>();
-        List<Token> tokens = new ArrayList<Token>();
-        
+            String pattern = "[a-z]{1}[a-z|0-9|_|-]*[@]" + ".*(hotmail|gmail|outlook|yahoo).*{1}[.]" + ".*(es|com|net|org|edu).*{1}";
+            boolean matches = Pattern.matches(pattern, "edulinaro@gmail.com");
+            System.out.println(matches);
+
+
+
+        List<String> palabras=new ArrayList<String>();
         String path ="./test.txt";
-        Lexer analizadorLexico = new Lexer();
+
         ReadFile fileReader = new ReadFile();
-        
         palabras=fileReader.ReadTxt(path);
-        //System.out.println(palabras);
-        for (String palabra : palabras) {
-            String token=analizadorLexico.tokenize(palabra);
-            if(token!=null){
-                tokens.add(new Token(token, palabra));
-            }else{
-                System.out.println("PAPAYA LEXICA");
-                tokens = new ArrayList<Token>();
-                break;
-            }
-        }
-        System.out.println("Tokens:");
-        for(Token token : tokens) {
-            System.out.print(token.t + ": " + token.c);
-            System.out.println("");
-        }
+        System.out.println(palabras);
+
     }
 }
