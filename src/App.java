c0dcff66ef23b9545c4b21def1959da4af980f81
Hernán Fonseca <hfonseca@excibit-corp.com>
2020-11-18 19:19:48 -0500

    Configurado el main para pasar las palabras al tokenizer

--- src/App.java
+++ src/App.java
@@ -7,20 +7,18 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        
-
-            String pattern = "[a-z]{1}[a-z|0-9|_|-]*[@]" + ".*(hotmail|gmail|outlook|yahoo).*{1}[.]" + ".*(es|com|net|org|edu).*{1}";
-            boolean matches = Pattern.matches(pattern, "edulinaro@gmail.com");
-            System.out.println(matches);
-
-
-
         List<String> palabras=new ArrayList<String>();
+        List<String> tokens=new ArrayList<String>();
+        
         String path ="./test.txt";
-
+        Lexer analizadorLexico = new Lexer();
         ReadFile fileReader = new ReadFile();
+        
         palabras=fileReader.ReadTxt(path);
-        System.out.println(palabras);
-
+        //System.out.println(palabras);
+        for (String palabra : palabras) {
+            String token=analizadorLexico.tokenize(palabra);
+            tokens.add(token);
+        }
     }
 }
