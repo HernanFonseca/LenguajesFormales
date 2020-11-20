c1e1d84bdc6047d4cfe87ada110052b9e339da1a
lcaliu23 <lucia.liu@utp.ac.pa>
2020-11-19 19:55:54 -0500

    sintax obligatorio en el orden nombre-mail-telefono

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class App {
 
-
     public static class Token {
         public final String t;
         public final String c; // contents
@@ -19,37 +18,40 @@ public class App {
         List<String> palabras = new ArrayList<String>();
         List<Token> tokens = new ArrayList<Token>();
         
-        String path ="./test.txt";
+        String path ="./10.txt";
         Lexer analizadorLexico = new Lexer();
         ReadFile fileReader = new ReadFile();
         
-        palabras=fileReader.ReadTxt(path);
+        palabras = fileReader.ReadTxt(path);
         //System.out.println(palabras);
         for (String palabra : palabras) {
-            String token=analizadorLexico.tokenize(palabra);
-            if(token!=null){
+            String token = analizadorLexico.tokenize(palabra);
+            if(token != null){
                 tokens.add(new Token(token, palabra));
-            }else{
+            } else {
                 System.out.println("ERROR LÉXICO");
                 tokens = new ArrayList<Token>();
                 break;
             }
         }
+
         System.out.println("Tokens:");
-        String previous="";
+        String previous = "";
         for(Token token : tokens) {
             System.out.print(token.t + ": " + token.c);
             System.out.println("");
-            if(token.t.startsWith("CONT") && !token.t.equals("CONTNOMBRE") && previous.startsWith("CONT")){
-                System.out.println("ERROR SINTÁCTICO EN EL TOKEN: "+token.t);
+            if( (previous.equals("") && token.t.equals("NOMBRE")) ||
+                ((previous.equals("NOMBRE") || previous.equals("CONTNOMBRE")) && token.t.equals("CONTNOMBRE")) ||
+                (previous.equals("CONTNOMBRE") && token.t.equals("EMAIL")) ||
+                (previous.equals("EMAIL") && token.t.equals("CONTEMAIL")) ||
+                (previous.equals("CONTEMAIL") && token.t.equals("TELEFONO")) ||
+                (previous.equals("TELEFONO") && token.t.equals("CONTTELEFONO"))) {
+            } else {
+                System.out.println("ERROR SINTÁCTICO EN EL TOKEN: " + token.t);
                 break;
             }
-            if(previous.equals("") && token.t.startsWith("CONT")){
-                System.out.println("ERROR SINTÁCTICO EN EL TOKEN: "+token.t);
-                break;
-            }
-            previous=token.t;
+            
+            previous = token.t;
         }
-        
     }
 }
